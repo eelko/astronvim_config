da272c857d8ce93dfa2f9e0216fa88ea12d7764e
Eelko <eelko@Eelkos-MacBook-Pro.local>
2023-11-30 09:22:44 +0100

    changed treesitter help to vimdoc

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'help', 'cmake', 'javascript', 'dockerfile' },
+    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'vimdoc', 'cmake', 'javascript', 'dockerfile' },
   },
 }
